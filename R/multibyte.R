62b16ab2682f14bc1961ecae17fd91a1fd98d599
kota <kmori05@gmail.com>
2016-09-18 00:33:32 +0900

    multibyte

---
+++ R/multibyte.R
@@ -0,0 +1,11 @@
+#' print multibyte
+#' @export
+hello3 <- function()
+{
+  print("\u6d4b\u8bd5")
+
+  # the following code tells you the right expression for your words:
+  # sprintf("%x", utf8ToInt("测试"))
+}
+
+
